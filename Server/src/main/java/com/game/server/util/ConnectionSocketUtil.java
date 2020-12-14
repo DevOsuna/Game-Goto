5fca3b90b518418f569c1be210d18f4ce7642106
DevOsuna <183462@ids.upchiapas.edu.mx>
2020-12-14 04:30:13 -0600

    Updated Direction

--- Server/src/main/java/com/game/server/util/ConnectionSocketUtil.java
+++ Server/src/main/java/com/game/server/util/ConnectionSocketUtil.java
@@ -3,5 +3,5 @@ package com.game.server.util;
 public class ConnectionSocketUtil {
 
     public static final int PORT = 5000;
-    public static final String HOST = "localhost";
+    //public static final String HOST = "200.77.66.227";
 }
